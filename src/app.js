36be38d70b79b3d4b0ab6a04aeff0d0720da812b
Camilo Figueroa <cigotete@gmail.com>
2024-03-22 22:37:49 -0500

    Changes on app.js to suport dynamic load of modules.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
-window.appJavascript = {
 
-  // método opcional para inicializar la aplicación
+const app = {
   async bootstrap(props){
     console.log("Inicio de la app js", props);
   },
 
-  // método obligatorio para montar la aplicación
   async mount(props) {
     console.log("App-montada:", props);
-    const contenedor = document.getElementById("app-mf-javascript");
+    const contenedor = document.getElementById("app-javascript");
     const boton = document.createElement("button");
-    boton.textContent = `Guardar ${props.nombre}`;
+    boton.innerHTML = "Click me!";
     contenedor.appendChild(boton);
   },
 
-  // método obligatorio para desmontar la aplicación
   async unmount(props) {
     console.log("App-desmontado", props);
-    const contenedor = document.getElementById("app");
+    const contenedor = document.getElementById("app-javascript");
     contenedor.innerHTML = "";
   }
 };
+
+export const { bootstrap, mount, unmount } = app;
